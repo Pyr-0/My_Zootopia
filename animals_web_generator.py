918329d889caa6c1d1c4eac6e4ce90154ebad1a6
pyro <miltonre2@gmail.com>
2025-03-31 18:25:47 +0200

    Final commit + bonus

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -18,35 +18,38 @@ def write_html(file_path, content):
 
 
 def serialize_animal(animal):
-	"""Cnverts a single animal dictionary to HTML"""
+	"""Converts a single animal dictionary to HTML"""
 
-	#Start with the opening list item tag
+	# Start with the opening list item tag
 	output = '<li class="cards__item">\n'
 
-	#Add the title(animal name)
-	output += f'	<div class="card__title">{animal["name"]}</div>\n'
+	# Add the title (animal name)
+	output += f'  <div class="card__title">{animal["name"]}</div>\n'
 
-	#add tthe paragraph for animal details
-	output += '	<p class="card__details">\n'
+	# Start the card text div
+	output += '  <div class="card__text">\n'
 
-	#Add the diet information
+	# Start a proper list for animal details
+	output += '	<ul class="animal-details">\n'
+
+	# Add diet information
 	if 'characteristics' in animal and 'diet' in animal['characteristics']:
-		output += f'		<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
+		output += f'	  <li class="detail-item"><strong>Diet:</strong> {animal["characteristics"]["diet"]}</li>\n'
+
 	# Add the first location
 	if 'locations' in animal and len(animal['locations']) > 0:
-		output += f'	  <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
+		output += f'	  <li class="detail-item"><strong>Location:</strong> {animal["locations"][0]}</li>\n'
 
 	# Add the type
 	if 'type' in animal:
-		output += f'	  <strong>Type:</strong> {animal["type"]}<br/>\n'
-
-	# Close the paragraph and list item
-	output += '  </p>\n'
+		output += f'	  <li class="detail-item"><strong>Type:</strong> {animal["type"]}</li>\n'
+	
+	# Close the lists and divs
+	output += '	</ul>\n'
+	output += '  </div>\n'
 	output += '</li>\n'
 
 	return output
-
-
 def generate_animal_html():
 	"""Main function to generate the animals HTML page"""
 	# Load the animal data
@@ -71,7 +74,3 @@ def generate_animal_html():
 # Run the main function
 if __name__ == "__main__":
 	generate_animal_html()
-
-""""Load JSON data"""
-animals_data = load_data("animals_data.json")
-
